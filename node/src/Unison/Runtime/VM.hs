a7fe8ac49a63dba8c8bf93ab37d6215e4648793b
Paul Chiusano <paul.chiusano@gmail.com>
2016-07-29 17:26:17 -0400

    rewrite of VM using faster approach - compile directly to program that manipulates mutable runtime stack

--- node/src/Unison/Runtime/VM.hs
+++ node/src/Unison/Runtime/VM.hs
@@ -1,178 +1,206 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TupleSections #-}
 
 module Unison.Runtime.VM where
 
-import Data.Foldable
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Data.IORef
 import Data.List
-import Data.Semigroup
-import GHC.Generics
 import Unison.Reference (Reference)
 import Unison.Runtime.Vector (Vector)
 import Unison.Term (Term)
 import Unison.Var (Var)
 import qualified Data.Text as Text
 import qualified Data.Vector as DV
+import qualified Data.Vector.Mutable as MV
 import qualified Unison.Reference as R
 import qualified Unison.Runtime.Vector as Vector
 import qualified Unison.Term as T
 
-newtype Program v builtin = Program [Instruction (Value (Program v) v builtin)] deriving (Show,Generic)
-
-type PureFn p v b = Machine p v b -> Machine p v b
-type ImpureFn p v b = Machine p v b -> IO (Machine p v b)
-
-type Stack p v builtin = DV.Vector (Value p v builtin)
-
-data Machine p v builtin
-  = Frame !(Stack p v builtin) !(Stack p v builtin) !(Maybe (Machine p v builtin))
-
-data Fn p builtin
-  = Builtin builtin
-  | Lam (p builtin) deriving Show
-
-data Value p v builtin
-  = Double !Double
+data Val v
+  = Number !Double
   | Text !Text.Text
-  | Var !(Maybe v)
-  | Vector !(Vector (Value p v builtin))
-  | Closure !Int !(Stack p v builtin) !(Fn p builtin) -- todo: [v] track variable names to use for decompile
-  deriving (Show,Generic)
+  | Symbol !(Maybe v)
+  | Vector !(Vector (Val v))
+  | Fn { arity :: !Int, relfect :: Program v (T.Term v), env :: !(Program v Int), invoke :: !(Program v ()) }
 
-data Instruction v
-  = Push !v
-  | Local !Int -- Push local variable onto work stack
-  | Call -- Call function at top of work stack, move work stack to local variable stack
-  | Return -- Reify work stack as a value, push it onto work stack of below frame, pop frame
-  | LetRec [v] [Instruction v]
-  deriving (Show,Generic,Functor,Traversable,Foldable)
+type Program v a = ReaderT (Stack (Val v)) IO a
 
-at :: Int -> Machine p v b -> Value p v b
-at n (Frame args _ bot) | n < DV.length args = case args DV.!? (DV.length args - 1 - n) of
-  Just v -> v
-  Nothing -> case bot of
-    Nothing -> error "invalid index"
-    Just f -> at (n - DV.length args) f
-
-zero :: Machine p v b
-zero = Frame DV.empty DV.empty Nothing
-
-traceInterpret
-  :: (Show builtin, Show v)
-  => (builtin -> Either (ImpureFn (Program v) v builtin) (PureFn (Program v) v builtin))
-  -> [Instruction (Value (Program v) v builtin)]
-  -> IO (Machine (Program v) v builtin)
-traceInterpret c prog = interpret' c (Just trace) prog zero where
-  trace i m = do
-    putStrLn (show i)
-    putStrLn (debugMachine m)
-
-interpret' :: (builtin -> Either (ImpureFn (Program v) v builtin) (PureFn (Program v) v builtin))
-           -> Maybe (Instruction (Value (Program v) v builtin) -> Machine (Program v) v builtin -> IO ())
-           -> [Instruction (Value (Program v) v builtin)]
-           -> Machine (Program v) v builtin
-           -> IO (Machine (Program v) v builtin)
-interpret' c trace prog machine = go0 prog machine where
-  go0 = case trace of
-    Nothing -> go
-    Just trace -> \p m -> case p of
-      [] -> go p m
-      h : _ -> trace h m >> go p m
-  go (Local n : prog) m@(Frame args work bot) = go0 prog (Frame args (work `DV.snoc` at n m) bot)
-  go (Push v : prog)    (Frame args work bot) = go0 prog (Frame args (work `DV.snoc` v) bot)
-  go (Call : prog) m@(Frame args provided bot) | DV.length provided >= 1 = case DV.unsafeLast provided of
-    Closure nRequired env fn
-      | nargs == nRequired -> case fn of -- nargs counts the closure itself, whereas nRequired doesn't
-        Builtin fn -> case c fn of
-          Left impure -> go0 prog =<< impure m
-          Right pure -> go0 prog (pure m)
-        Lam (Program fn) -> go0 (fn ++ prog) (Frame args' DV.empty bot') where
-          args' = DV.concat [env, DV.init provided]
-          bot' = Just (Frame args DV.empty bot)
-      | nargs > nRequired -> -- over-application, like `id id 42`
-        let
-          provided' = DV.take nRequired provided
-          extra = Push <$> DV.toList (DV.drop nRequired provided)
-        in go0 ((Call : extra) ++ prog) (Frame args (provided' `DV.snoc` Closure nRequired env fn) bot)
-      | nargs < nRequired ->  -- under-application `(+) 1`
-        go0 prog (Frame args (DV.singleton $ Closure (nRequired-nargs) (DV.concat[env,provided]) fn) bot)
-    _ -> error "attempt to apply non-function"
-    where nargs = DV.length provided - 1 -- don't include the Closure itself, only the arguments to it
-  go [] stack = pure stack
-  go (Return : prog) (Frame _ work bot) = case maybe zero id bot of
-    Frame args work2 bot -> go0 prog (Frame args (DV.concat [work2,work]) bot)
-  --go (LetRec [binding] body : prog) m =
-  --  let binding' = Suspend . Program $ [Push binding, Push binding, Call, Return]
-  --  -- body can refer to itself via Local 0
-  --  in go0 (Push binding' : (Push . Suspend . Program $ body) : Call : Return : prog) m
-  --  -- generalize this to two bindings, etc
-  --go _ Bottom = error "Bottom machine state"
-  go (Call : _) _ = error "Call on Empty work stack"
-
-newtype Builder v = Builder ([Instruction v] -> [Instruction v])
-
-build :: Builder v -> [Instruction v]
-build (Builder b) = b []
-
-instance Monoid (Builder v) where
-  mempty = Builder id
-  Builder f `mappend` Builder g = Builder (f . g)
-
-instance Semigroup (Builder v) where
-  (<>) = mappend
-
-one :: Instruction v -> Builder v
-one slot = Builder (slot:)
-
-thunk :: [Instruction (Value (Program v1) v1 builtin)] -> Value (Program v1) v builtin
-thunk = Closure 0 DV.empty . Lam . Program
-
-compile :: Var v => Term v -> [Instruction (Value (Program v) v Reference)]
-compile = build . compile'
-
-compile' :: Var v => Term v -> Builder (Value (Program v) v Reference)
-compile' t = go [] t where
-  go vs t = case t of
-    T.Blank' -> one (Push $ Var Nothing)
-    T.Ref' r -> one (Push . Closure 0 DV.empty . Builtin $ r)
-    T.Lit' l -> case l of
-      T.Number n -> one (Push $ Double n)
-      T.Text txt -> one (Push $ Text txt)
-    T.Ann' x _ -> go vs x
-    -- todo: strictly evaluate elements
-    T.Vector' xs -> one (Push $ Vector (Vector.fromList $ map (thunk . build . go vs) (toList xs)))
-    T.Apps' f args -> mconcat [ go vs x | x <- args ] <> go vs f <> one Call
-    T.Var' v -> case elemIndex v vs of
-      Just i -> one (Local i)
-      Nothing -> one (Push $ Var (Just v))
-    T.LamsNamed' args body -> one (Push . Closure (length args) DV.empty . Lam . Program $ (build (go (reverse args ++ vs) body <> one Return)))
-    T.Let1Named' v b body -> go vs b
-                          <> one (Push . thunk $ (build (go (v:vs) body <> one Return)))
-                          <> one Call
-    T.LetRecNamed' bs body -> one (LetRec bs' body') where
-      vs' = reverse (map fst bs) ++ vs
-      bs' = [ thunk $ (build (go vs' b <> one Return)) | (_,b) <- bs ]
-      body' = build (go vs' body)
-    _ -> error $ "do not know how to compile: " ++ show t
+data Stack a =
+  Stack { topIndex :: IORef Int
+        , values :: IORef (MV.IOVector a)
+        , clearValue :: a }
 
 {-
-builtins ref = case ref of
-  R.Derived _ -> error "no go"
-  R.Builtin r -> case Text.unpack r of
-    "Number.plus" -> Right $ \(Frame args (Cons self (Cons y (Cons x tl))) bot) -> case (x, y) of
-      (Double x, Double y) -> Frame args (Cons (Double (x+y)) tl) bot
-      _ -> Frame args (Cons (Suspend (Program [Push x, Push y, Push self, Call])) tl) bot
-    "Number.minus" -> Right $ \(Frame args (Cons self (Cons y (Cons x tl))) bot) -> case (x, y) of
-      (Double x, Double y) -> Frame args (Cons (Double (x-y)) tl) bot
-      _ -> Frame args (Cons (Suspend (Program [Push x, Push y, Push self, Call])) tl) bot
-    r -> error $ "unknown ref " ++ r
-
+stack0 :: IO (Stack (Val v))
+stack0 = do
+  i <- newIORef 0
+  v <- MV.new 128
+  values
 -}
 
-debugMachine :: (Show v, Show b) => Machine (Program v) v b -> String
-debugMachine (Frame args work bot) =
-  show args ++ " " ++ show work ++ "\n" ++
-  (maybe "" debugMachine bot)
+top :: Program v (Val v)
+top = ask >>= \s -> liftIO $ top' s
+
+push :: Val v -> Program v ()
+push v = ask >>= \s -> liftIO $ push' s v
+
+pop :: Program v (Val v)
+pop = ask >>= \s -> liftIO $ pop' s
+
+popFrame' :: Int -> Program v (Val v)
+popFrame' args = popFrame args >> pop
+
+-- Saves the top element of the stack, drops the number of arguments from the
+-- stack, then repushes the top element; popFrame 2 [a,b,c,d] == [a,d]
+popFrame :: Int -> Program v ()
+popFrame args = do
+  s <- ask
+  top <- liftIO $ pop' s
+  liftIO $ drop' args s
+  liftIO $ push' s top
+
+at :: Int -> Program v (Val v)
+at i = ask >>= \s -> liftIO $ at' i s
+
+top' :: Stack a -> IO a
+top' (Stack i vs _) = do
+  i <- readIORef i
+  vs <- readIORef vs
+  MV.read vs i
+
+at' :: Int -> Stack a -> IO a
+at' j (Stack i vs _) = do
+  i <- readIORef i
+  vs <- readIORef vs
+  MV.read vs (i-j)
+
+push' :: Stack a -> a -> IO ()
+push' s@(Stack ir vsr _) !a = do
+  i <- readIORef ir
+  vs <- readIORef vsr
+  case i >= MV.length vs - 1 of
+    False -> let !i' = i+1 in MV.write vs i' a >> writeIORef ir i'
+    True -> do
+      vs <- MV.grow vs (MV.length vs)
+      writeIORef vsr vs
+      push' s a
+
+pop' :: Stack a -> IO a
+pop' (Stack ir vsr null) = do
+  i <- readIORef ir
+  vs <- readIORef vsr
+  a <- MV.read vs i
+  MV.write vs i null
+  let !i' = (i - 1) `max` 0
+  writeIORef ir i'
+  pure a
+
+drop' :: Int -> Stack a -> IO ()
+drop' n (Stack ir vsr null) = do
+  i <- readIORef ir
+  vs <- readIORef vsr
+  MV.write vs i null
+  let !i' = (i - n) `max` 0
+  writeIORef ir i'
+
+compile' :: Var v => (R.Reference -> Program v ()) -> Term v -> Program v ()
+compile' link t = go [] t where
+  go vs t = case t of
+    T.Blank' -> push (Symbol Nothing)
+    T.Lit' l -> case l of
+      T.Number n -> push (Number n)
+      T.Text txt -> push (Text txt)
+    T.Ref' r -> link r
+    T.Ann' x _ -> go vs x
+    T.Var' v -> case elemIndex v vs of
+      Just i -> at i >>= push
+      Nothing -> push (Symbol (Just v))
+    T.Vector' xs ->
+      let !xs' = fmap (\x -> go vs x >> pop) (Vector.fromList . DV.toList $ xs)
+      in do
+        xs <- sequenceA xs'
+        push $ Vector xs
+    T.Let1Named' v b body -> do b'; body'; popFrame 1 where
+      !b' = go vs b
+      !body' = go (v:vs) body
+    T.LamsNamed' args body -> push fn where
+      !body' = go (reverse args ++ vs) body
+      !arity = length args
+      !fn = Fn arity (pure (T.lam'' args body)) (pure 0) body'
+    T.LetRecNamed' bs body ->
+      let
+        !vs' = reverse (map fst bs) ++ vs
+        !n = length bs
+        !bs' = [go vs' b >> popFrame' n | (_,b) <- bs]
+        !body' = go vs' body
+      in mdo
+        results <- forM bs' (\b -> mapM_ push results >> b)
+        mapM_ push results
+        body'
+        popFrame n
+    -- T.Apps' (T.LamsNamed' argnames body) args -- todo: give optimized impl?
+    T.Apps' f args ->
+      let
+        !ef = eval vs f
+        !eargs = forM_ args (go vs)
+        !nargs = length args
+        dargs = DV.map (eval vs) (DV.fromList args)
+        deoptimized = do f <- ef; applied <- foldM app f (DV.toList dargs); push applied where
+          app (Fn arity reify env invoke) arg = arg >>= \arg ->
+            if arity == 1 then do n <- env; push arg; invoke; popFrame (n+1); pop
+            else pure (Fn (arity-1) (reify2 arg) ((1+) <$> (env <* push arg)) invoke)
+            where
+            reify2 arg = do
+              f <- reify
+              arg <- decompile arg
+              pure $ T.app f arg
+          app _ _ = error $ "application of non-function"
+      in do
+        Fn arity reify env prog <- ef
+        case nargs of
+          _ | nargs == arity -> env >>= \n -> eargs >> prog >> popFrame (nargs+n) -- fully saturated call
+            | nargs > arity -> deoptimized -- over-application, ex: id id 42
+            | otherwise -> do -- nargs < arity, under-application, need to form a closure
+                -- Example: let f x y z = x - y - z; g = f 12 2 in g 1 -- should be 9
+                eargs -- evaluate the args
+                env' <- reverse <$> replicateM nargs pop -- extract the environment
+                -- create the closure from the argument
+                push (Fn (arity-nargs) (reify2 env') ((+) <$> env <*> (nargs <$ forM_ env' push)) prog)
+                where
+                reify2 env' = do
+                  f <- reify
+                  args <- mapM decompile env'
+                  pure $ T.apps f args
+    _ -> error $ "don't know what to do with: " ++ show t
+  eval vs t = go vs t >> pop
+
+decompile :: Ord v => Val v -> Program v (T.Term v)
+decompile val = case val of
+  Number n -> pure $ T.num n
+  Text txt -> pure $ T.text txt
+  Symbol Nothing -> pure $ T.blank
+  Symbol (Just v) -> pure $ T.var v
+  Vector vals -> T.vector . Vector.toList <$> traverse decompile vals
+  Fn _ decompile _ _ -> decompile
+
+popDecompile :: Ord v => Program v (T.Term v)
+popDecompile = decompile =<< pop
+
+builtins :: Ord v => R.Reference -> Program v ()
+builtins =
+  let
+    plus' = do Number x <- at 1; Number y <- at 0; push (Number (x+y))
+    minus' = do Number x <- at 1; Number y <- at 0; push (Number (x-y))
+    f2 r prog = Fn 2 (pure (T.ref r)) (pure 0) prog
+    plus = f2 (R.Builtin "Number.plus") plus'
+    minus = f2 (R.Builtin "Number.minus") minus'
+  in \ref -> case ref of
+    R.Derived _ -> error "no go"
+    R.Builtin r -> case Text.unpack r of
+      "Number.plus" -> push plus
+      "Number.minus" -> push minus
+      _ -> error "unknown"
